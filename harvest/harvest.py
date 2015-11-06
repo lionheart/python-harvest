eaf0663fb8f5a27076b82cb4e26bf9938c8d26d8
maartenkling <maarten@fourdigits.nl>
2015-11-06 20:49:38 +0100

    try and work on tasks

--- harvest/harvest.py
+++ harvest/harvest.py
@@ -6,24 +6,30 @@
 
 """
 
-import requests
-from requests_oauthlib import OAuth2Session
+import urllib2
+import json
 from urlparse import urlparse
 from base64   import b64encode as enc64
 
 HARVEST_STATUS_URL = 'http://www.harveststatus.com/api/v2/status.json'
 
+
 class HarvestError(Exception):
     pass
 
+
 class Harvest(object):
     def __init__(self, uri, email=None, password=None, client_id=None, token=None, put_auth_in_header=True):
-        self.__uri      = uri.rstrip('/')
+        self.__uri = uri.rstrip('/')
         parsed = urlparse(uri)
         if not (parsed.scheme and parsed.netloc):
             raise HarvestError('Invalid harvest uri "{0}".'.format(uri))
 
         self.__headers = {
+            # 'Accept': 'application/xml',
+            # 'Content-Type': 'application/xml',
+            # 'User-Agent': 'harvest.py',
+            'Content-Type'  : 'application/json',
             'Accept'        : 'application/json',
             'User-Agent'    : 'Mozilla/5.0',  # 'TimeTracker for Linux' -- ++ << >>
         }
@@ -41,18 +47,23 @@ class Harvest(object):
     @property
     def uri(self):
         return self.__uri
+
     @property
     def auth(self):
         return self.__auth
+
     @property
     def email(self):
         return self.__email
+
     @property
     def password(self):
         return self.__password
+
     @property
     def client_id(self):
         return self.__client_id
+
     @property
     def token(self):
         return self.__token
@@ -108,7 +119,8 @@ class Harvest(object):
         return self._get('/clients/{0}'.format(client_id))
 
     def create_client(self, new_client_id, name, **kwargs):
-        url  = '/clients/{0}'.format(new_client_id)
+        # url  = '/clients/{0}'.format(new_client_id)
+        url = '/clients/'
         kwargs.update({'name':name})
         return self._post(url, data=kwargs)
 
@@ -142,6 +154,57 @@ class Harvest(object):
 
     def delete_project(self, project_id):
         return self._delete('/projects/{0}'.format(project_id))
+
+    ## Tasks
+
+    def tasks(self):
+        return self._get('/tasks')
+
+    def get_tasks(self, task_id):
+        return self._get('/tasks/{0}'.format(task_id))
+
+    def create_task(self, **kwargs):
+        # CREATE NEW TASK
+        return self._post('/tasks/', **kwargs)
+
+    def update_task(self, tasks_id, **kwargs):
+        # UPDATE AN EXISTING TASK
+        url = '/tasks/{0}'.format(tasks_id)
+        return self._put(url, data=kwargs)
+
+    def delete_task(self, tasks_id):
+        # ARCHIVE OR DELETE EXISTING TASK
+        # Returned if task does not have any hours associated - task will be deleted.
+        # Returned if task is not removable - task will be archived.
+        return self._delete('/tasks/{0}'.format(tasks_id))
+
+    def activate_task(self, tasks_id):
+        # ACTIVATE EXISTING ARCHIVED TASK
+        return self._delete('/tasks/{0}/activate'.format(tasks_id))
+
+    ## Task Assignment: Assigning tasks to projects
+
+    # GET ALL TASKS ASSIGNED TO A GIVEN PROJECT
+    # /projects/#{project_id}/task_assignments
+
+    # GET ONE TASK ASSIGNMENT
+    # GET /projects/#{project_id}/task_assignments/#{task_assignment_id}
+
+    # ASSIGN A TASK TO A PROJECT
+    # POST /projects/#{project_id}/task_assignments
+
+    def create_tasks_to_project(self, project_id, name, **kwargs):
+        # CREATE A NEW TASK AND ASSIGN IT TO A PROJECT
+        # POST /projects/#{project_id}/task_assignments/add_with_create_new_task
+        kwargs.update({'name': name})
+        return self._post('/projects/{0}/task_assignments/add_with_create_new_task'.format(project_id), kwargs)
+
+    # REMOVING A TASK FROM A PROJECT
+    # DELETE /projects/#{project_id}/task_assignments/#{task_assignment_id}
+    #
+    #
+    # CHANGING A TASK FOR A PROJECT
+    # PUT /projects/#{project_id}/task_assignments/#{task_assignment_id}
 
     ## Expense Categories
 
@@ -190,38 +253,40 @@ class Harvest(object):
 
     def _get(self, path='/', data=None):
         return self._request('GET', path, data)
+
     def _post(self, path='/', data=None):
         return self._request('POST', path, data)
+
     def _put(self, path='/', data=None):
         return self._request('PUT', path, data)
+
     def _delete(self, path='/', data=None):
         return self._request('DELETE', path, data)
-    def _request(self, method='GET', path='/', data=None):
-        url = '{self.uri}{path}'.format(self=self, path=path)
-        kwargs = {
-            'method'  : method,
-            'url'     : '{self.uri}{path}'.format(self=self, path=path),
-            'headers' : self.__headers,
-            'data'    : data,
-        }
-        if self.auth == 'Basic':
-            requestor = requests
-            if 'Authorization' not in self.__headers:
-                kwargs['auth'] = (self.email, self.password)
-        elif self.auth == 'OAuth2':
-            requestor = OAuth2Session(client_id=self.client_id, token=self.token)
 
+    def _request(self, method='GET', path='/', data=None):
         try:
-            resp = requestor.request(**kwargs)
+            print data
+            print path
+            print self
+            req = urllib2.Request('{self.uri}{path}'.format(self=self, path=path), data=data, headers=self.__headers)
+            print req
+            resp = urllib2.urlopen(req)
+            print resp
             if 'DELETE' not in method:
-                return resp.json()
+                try:
+                    return json.loads(resp.read())
+                except:
+                    return resp
             return resp
         except Exception, e:
+            print 'errrororrrr'
+            print e
             raise HarvestError(e)
+
 
 def status():
     try:
-        status = requests.get(HARVEST_STATUS_URL).json().get('status', {})
+        status = json.loads(urllib2.urlopen(HARVEST_STATUS_URL).read()).get('status', {})
     except:
         status = {}
     return status
