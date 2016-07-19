b5ddd1498dd70594b3d849899602f9a90612dfbf
Dan Loewenherz <dan@lionheartsw.com>
2016-07-19 13:54:04 -0500

    clean up setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
-from setuptools import setup, find_packages
-import sys, os
+#!/usr/bin/env python
+
+import os
+from setuptools import find_packages
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 # Utility function to read the README file.
 # Used for the long_description.  It's nice, because now 1) we have a top level
@@ -8,28 +15,33 @@ import sys, os
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+metadata = {}
+execfile("bigstore/metadata.py", metadata)
+
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Environment :: Console",
+    "Intended Audience :: Developers",
+    "Natural Language :: English",
+    "Operating System :: POSIX :: Linux",
+    "Programming Language :: Python :: 2.7",
+    "Topic :: Software Development :: Libraries",
+    "Topic :: Internet :: WWW/HTTP :: Site Management",
+    "Topic :: Utilities",
+    "License :: OSI Approved :: MIT License",
+]
+
 setup(
     name='python-harvest-redux',
-    version=read('VERSION'),
+    version=metadata['__version__'],
     description="Harvest API client",
-    long_description=read('README'),
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
-        "Intended Audience :: Developers",
-        "Natural Language :: English",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python :: 2.7",
-        "Topic :: Software Development :: Libraries",
-        "Topic :: Internet :: WWW/HTTP :: Site Management",
-        "Topic :: Utilities",
-        "License :: OSI Approved :: MIT License",
-    ],
+    long_description="Harvest Time Tracking API Client"
+    classifiers=classifiers,
     keywords='harvestapp timetracking api',
-    author='Alex Goretoy',
-    author_email='alex@goretoy.com',
+    author=metadata['__author__'],
+    author_email=metadata['__email__'],
     url='https://github.com/lionheart/python-harvest',
-    license='MIT License',
+    license=metadata['__license__'],
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     include_package_data=True,
     zip_safe=True,
