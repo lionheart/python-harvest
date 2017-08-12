a865bc2c45ae4240c41087c3496f9853049209ae
Dan Loewenherz <dan@lionheartsw.com>
2017-08-12 18:39:09 -0500

    update license info

--- harvest/harvest.py
+++ harvest/harvest.py
@@ -1,3 +1,17 @@
+# Copyright 2012-2017 Lionheart Software LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import json
 import requests
 from requests_oauthlib import OAuth2Session
@@ -5,7 +19,6 @@ from urlparse import urlparse
 from base64   import b64encode as enc64
 
 HARVEST_STATUS_URL = 'http://www.harveststatus.com/api/v2/status.json'
-
 
 class HarvestError(Exception):
     pass
@@ -17,7 +30,7 @@ class Harvest(object):
         parsed = urlparse(uri)
         if not (parsed.scheme and parsed.netloc):
             raise HarvestError('Invalid harvest uri "{0}".'.format(uri))
-            
+
         if refresh_token:
             self.__headers = {
                 "Content-Type": 'application/x-www-form-urlencoded',
--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python
 
+# Copyright 2012-2017 Lionheart Software LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import runpy
 import os
 from setuptools import find_packages
 
@@ -15,8 +30,8 @@ except ImportError:
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-metadata = {}
-execfile("harvest/metadata.py", metadata)
+metadata_filename = "harvest/metadata.py"
+metadata = runpy.run_path(metadata_filename)
 
 # http://pypi.python.org/pypi?:action=list_classifiers
 classifiers = [
