cf43f03a8c71da972e74f3a3df26f59e296f68e9
Dan Loewenherz <dan@lionheartsw.com>
2018-02-07 14:08:06 -0600

    bump version to 3.1.1

--- harvest/metadata.py
+++ harvest/metadata.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "3.1.0"
+__version__ = "3.1.1"
 __author__ = "Alex Goretoy"
 __copyright__ = "Copyright 2012-2018, Lionheart Software LLC"
 __maintainer__ = "Dan Loewenherz"
